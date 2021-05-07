1b67c0d21108d76248bfb83e0e18aa243aea9e50
Leonardo Alves <leoroberto.alves@hotmail.com>
2021-05-07 11:06:57 -0300

    Include Hooks and Contexts to switch theme on application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { AppProps } from "next/app";
 
-import GlobalStyle from "../styles/global";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "../contexts/ThemeContext";
 import { Wrapper } from "../styles/pages/App";
-
-import * as themes from "../styles/theme";
 
 import { Header } from "../components/Header";
 import Player from "../components/Player";
 import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState(themes.light);
-
-  function toggleTheme() {
-    setTheme(theme.title === "light" ? themes.dark : themes.light);
-  }
-
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <PlayerContextProvider>
-        <GlobalStyle />
         <Wrapper>
           <main>
-            <Header toggleTheme={toggleTheme} />
+            <Header />
             <Component {...pageProps} />
           </main>
           <Player />
